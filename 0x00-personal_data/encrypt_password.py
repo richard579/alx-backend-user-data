c4850c942ce1101c686f43789cb000bfaf9c1166
richard579 <richardbeng358@outlook.com>
2024-02-07 13:47:52 +0000

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Returns a salted, hashed password, byte in string"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns byte string password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hash_password: bytes, password: str) -> bool:
+    """Implement an is_valid to validate that the provided password 
+    matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hash_password)
