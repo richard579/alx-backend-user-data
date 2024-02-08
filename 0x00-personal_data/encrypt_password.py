59710f99c6e192f810fd5c5fb68d33903c59cb87
richard579 <richardbeng358@outlook.com>
2024-02-08 15:46:39 +0000

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-"""Returns a salted, hashed password, byte in string"""
+"""
+Returns the log message obfuscated
+"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Returns byte string password"""
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-    return hashed
+    """Hash password using bcrypt package
+    to perform the hashing with hashpw
+    """
+    # Convert password to bytes
+    byte_pass = password.encode('utf-8')
+    # Generate salt to add to password
+    gen_salt = bcrypt.gensalt()
+
+    hash_pass = bcrypt.hashpw(byte_pass, gen_salt)
+    return hash_pass
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Implement an is_valid to validate that the provided password 
-    matches the hashed password."""
-    valid = False
-    encoded = password.encode()
-    if bcrypt.checkpw(encoded, hashed_password):
-        valid = True
-    return valid
+    """Validate that the provided password
+    matches the hashed password
+    """
+    # Convert password to bytes
+    bytes_pass = password.encode('utf-8')
+    is_val = bcrypt.checkpw(bytes_pass, hashed_password)
+    return is_val
