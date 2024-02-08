88e15571421f926f655b1861707be03043d8f679
richard579 <richardbeng358@outlook.com>
2024-02-08 13:30:29 +0000

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,10 +5,16 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Returns byte string password"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Implement an is_valid to validate that the provided password 
     matches the hashed password."""
-    return bcrypt.checkpw(password.encode('utf-8'), hash_password)
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid
