600b47bffd8785d48f46dd223093e214c5c995af
richard579 <richardbeng358@outlook.com>
2024-02-08 12:51:33 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,10 +12,12 @@ import mysql.connector
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Returns regex obfuscated log messages"""
-    for f in fields:
-        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returns a log message obfuscated."""
+    for field in fields:
+        message = re.sub(rf"{field}=(.*?){separator}",
+                         f"{field}={redaction}{separator}", message)
     return message
 
 
