0044eca2da7041208b946608442ee61cf86cd45a
richard579 <richardbeng358@outlook.com>
2024-02-15 17:49:20 +0000

    user_session.py

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""user session module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """User session class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """constructor method"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
