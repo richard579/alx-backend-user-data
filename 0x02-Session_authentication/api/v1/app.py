8b77ad905c9d3b7cddcb9d451df4a470f1e7929a
richard579 <richardbeng358@outlook.com>
2024-02-15 11:53:24 +0000

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
 
 app = Flask(__name__)
@@ -64,13 +64,13 @@ def before_request() -> str:
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/'
+                      '/api/v1/forbidden/',
                       '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
             and auth.session_cookie(request) is None:
         abort(401)
 
